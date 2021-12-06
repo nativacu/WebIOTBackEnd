ecd3cd9a0bf8f253b818fb87c489ee4454526496
manuelmolinaf <manuelmolinaf@outlook.com>
2021-12-05 23:36:08 -0400

    ༼ つ ◕_◕ ༽つ

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
+from flask import Flask
+from awscrt import io, mqtt, auth, http
+from awsiot import mqtt_connection_builder
+import time as t
+import json
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Define ENDPOINT, CLIENT_ID, PATH_TO_CERTIFICATE, PATH_TO_PRIVATE_KEY, PATH_TO_AMAZON_ROOT_CA_1, MESSAGE, TOPIC, and RANGE
+ENDPOINT = "a12ejgff2slvq5-ats.iot.us-east-1.amazonaws.com"
+CLIENT_ID = "testDevice"
+PATH_TO_CERTIFICATE = "certificates/b02985cd7313a9e9b189ae9bf97289a66082dd83c15a58f5370a827da023c05a-certificate.pem.crt"
+PATH_TO_PRIVATE_KEY = "certificates/b02985cd7313a9e9b189ae9bf97289a66082dd83c15a58f5370a827da023c05a-private.pem.key"
+PATH_TO_AMAZON_ROOT_CA_1 = "certificates/AmazonRootCA1.pem"
+
+app = Flask(__name__)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+@app.route("/crossMessage")
+def hello_world():
+    MESSAGE = "cross"
+    TOPIC = "semaphore"
+    # Spin up resources
+    event_loop_group = io.EventLoopGroup(1)
+    host_resolver = io.DefaultHostResolver(event_loop_group)
+    client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
+    mqtt_connection = mqtt_connection_builder.mtls_from_path(
+                endpoint=ENDPOINT,
+                cert_filepath=PATH_TO_CERTIFICATE,
+                pri_key_filepath=PATH_TO_PRIVATE_KEY,
+                client_bootstrap=client_bootstrap,
+                ca_filepath=PATH_TO_AMAZON_ROOT_CA_1,
+                client_id=CLIENT_ID,
+                clean_session=False,
+                keep_alive_secs=6
+                )
+    print("Connecting to {} with client ID '{}'...".format(
+            ENDPOINT, CLIENT_ID))
+    # Make the connect() call
+    connect_future = mqtt_connection.connect()
+    # Future.result() waits until a result is available
+    connect_future.result()
+    print("Connected!")
+    # Publish message to server desired number of times.
+    print('Begin Publish')
 
+    # data = "{} [{}]".format(MESSAGE, i + 1)
+    message = {"message": MESSAGE}
+    mqtt_connection.publish(topic=TOPIC, payload=json.dumps(message), qos=mqtt.QoS.AT_LEAST_ONCE)
+    print("Published: '" + json.dumps(message) + "' to the topic: " + TOPIC)
+    t.sleep(0.1)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('Publish End')
+    disconnect_future = mqtt_connection.disconnect()
+    disconnect_future.result()
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
